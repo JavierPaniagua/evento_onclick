2a55b3cc4df045c9da858815bb8b4e63c802b455
Javier <profejavy@gmail.com>
2022-12-12 20:06:06 -0300

    Primer Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+/* Cambiar texto */
+
+function turnOff(element) {
+    element.innerText ="Logout"
+}
+
+// eliminar boton
+
+function hide(element){
+    element.remove();
+}
+
+// like me gusta
+
+function like(){
+    alert('A Ninja le gusto');
+}
